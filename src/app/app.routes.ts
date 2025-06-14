df0c1bc67c160e86549661cab1853cf2b3c9a740
m1a2n3o4 <manumano111@gmail.com>
2025-06-14 16:55:00 +0530

    Dashboard page Design and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page-component/welcome-page-component';
 import { SkeltonLoaderComponent } from './shared/components/skelton-loader-component/skelton-loader-component';
+import { DashboardComponent } from './pages/dashboard-component/dashboard-component';
 
 export const routes: Routes = [
     { path: '', component: WelcomePageComponent },
-    { path: 'skelton', component: SkeltonLoaderComponent}
+    { path: 'skelton', component: SkeltonLoaderComponent},
+    { path: 'dashboard', component: DashboardComponent}
 ];
--- src/app/pages/dashboard-component/dashboard-component.ts
+++ src/app/pages/dashboard-component/dashboard-component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header-component/header-component';
+import { ProductListComponent } from '../../shared/components/product-list-component/product-list-component';
 
 @Component({
   selector: 'app-dashboard-component',
-  imports: [],
+  standalone: true,
+  imports: [HeaderComponent, ProductListComponent],
   templateUrl: './dashboard-component.html',
   styleUrl: './dashboard-component.css'
 })
